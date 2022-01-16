d700d7a0fbd53adfa37c347a50ad0b44a7d15ce0
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-01-15 22:30:33 -0600

    1157

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -10,7 +10,26 @@ function Pin() {
                 <div className='pin_title'></div>
 
                 <div className='pin_modal'>
+                    <div className='modal_head'>
+                        <div className='save_card'>Save</div>
+                    </div>
 
+                    <div className='modal_foot'>
+                        <div className='destination'>
+                            <div className='pint_mock_icon_container'>
+                                <img src='./images/upper-right-arrow.png' alt='destination' className='pint_mock_icon' />
+                            </div>
+                            <span>Eatery</span>
+                        </div>
+
+                        <div className='pint_mock_icon_container'>
+                            <img src='./images/send.png' alt='send' className='pint_mock_icon' />
+                        </div>
+
+                        <div className='pint_mock_icon_container'>
+                            <img src='./images/ellipse.png' alt='edit' className='pint_mock_icon' />
+                        </div>
+                    </div>
                 </div>
 
                 <div className='pin_image'>
